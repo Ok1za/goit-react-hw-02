a9a58c693c11d1d34ebad61f36028205cff47baa
Ok1za <oksanaprescott@gmail.com>
2024-03-25 18:45:17 +0200

    add Description doc

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,36 @@ import React, { useState, useEffect } from 'react';
 import Options from './components/Options/Options';
 import Feedback from './components/Feedback/Feedback';
 import Notification from './components/Notification/Notification';
+import Description from './components/Description/Description';
 
 
 const App = () => {
     const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0
+        good: 0,
+        neutral: 0,
+        bad: 0
     });
 
     useEffect(() => {
-    const storedFeedback = localStorage.getItem('feedback');
-    if (storedFeedback) {
-        setFeedback(JSON.parse(storedFeedback));
-    }
-    }, []);
+        const storedFeedback = localStorage.getItem('feedback');
+            if (storedFeedback) {
+                setFeedback(JSON.parse(storedFeedback));
+            }
+        }, []);
 
     const updateFeedback = feedbackType => {
-    setFeedback(prevState => ({
-        ...prevState,
-        [feedbackType]: prevState[feedbackType] + 1
-    }));
+        setFeedback(prevState => ({
+            ...prevState,
+            [feedbackType]: prevState[feedbackType] + 1
+        }));
     }
 
     const resetFeedback = () => {
-    setFeedback({ good: 0, neutral: 0, bad: 0 });
+        setFeedback({ good: 0, neutral: 0, bad: 0 });
     }
 
     useEffect(() => {
-    localStorage.setItem('feedback', JSON.stringify(feedback));
+        localStorage.setItem('feedback', JSON.stringify(feedback));
     }, [feedback]);
 
     const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
@@ -39,7 +40,7 @@ const App = () => {
     return (
     <div>
         <h1>Sip Happens Café</h1>
-        <p>Please leave your feedback about our service by selecting one of the options below.</p>
+        <Description />
         {totalFeedback === 0 ? (
         <Notification message="No feedback yet" />
         ) : (
---
+++ src/components/Description/Description.jsx
@@ -0,0 +1,9 @@
+const Description = () => {
+    return (
+    <div>
+        <p>Please leave your feedback about our service by selecting one of the options below.</p>
+    </div>
+    );
+}
+
+export default Description;
